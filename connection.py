8f5640da79b40d79c4a81058defdd1dffd0f92a2
PhylibhertBINGLEY <107876092+PhylibhertBINGLEY@users.noreply.github.com>
2025-01-07 00:15:23 +0100

    Test de connection

--- connection.py
+++ connection.py
@@ -5,6 +5,17 @@ from dotenv import load_dotenv
 # Charger le token
 load_dotenv()
 notion_token = os.getenv("NOTION_TOKEN")
+database_id = os.getenv("NOTION_DB_ID")
 
 # Initialiser le client Notion
 notion = Client(auth=notion_token)
+
+try:
+    # Récupérer les détails de la base
+    database = notion.databases.retrieve(database_id=database_id)
+    print("Connexion à la base réussie !")
+    print(f"Nom de la base : {database['title'][0]['plain_text']}")
+except Exception as e:
+    print("Erreur lors de la connexion :")
+    print(type(e).__name__)  # Type de l'exception
+    print(e)  # Message détaillé de l'erreur
