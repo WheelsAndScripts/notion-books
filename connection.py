2c383ed7a60b8b9ebe31bce9c0437af1794c2a2c
PhylibhertBINGLEY <107876092+PhylibhertBINGLEY@users.noreply.github.com>
2025-01-10 23:44:31 +0100

    Insertion des livres dans MongoDB réussie

--- connection.py
+++ connection.py
@@ -13,6 +13,8 @@ mongo_uri = os.getenv("MONGO_URI")
 
 # Initialiser le client Notion
 notion = Client(auth=notion_token)
+# Récupérer les livres
+response = notion.databases.query(database_id=database_id)
 
 try:
     # Récupérer les détails de la base
@@ -41,56 +43,6 @@ except Exception as e:
     print("Erreur lors de la récupération des colonnes :", e)
 '''
 
-
-# Récupérer les livres
-response = notion.databases.query(database_id=database_id)
-for result in response["results"]:
-    # Pour récupérer les titres des livres
-    # title = result["properties"]["Titre"]["title"][0]["text"]["content"]
-    title_property = result["properties"].get("Titre", {}).get("title", [])
-    title = title_property[0].get("text", {}).get("content", "No title") if title_property else "No title"
-
-    # Pour récupérer les auteurs des livres
-    # author = result["properties"]["Auteur"]["rich_text"][0]["text"]["content"]
-    author_property = result["properties"].get("Auteur", {}).get("rich_text", [])
-    author = author_property[0].get("text", {}).get("content", "No author") if author_property else "No author"
-
-    # Pour récupérer les tags/genres des livres
-    tags = result["properties"].get("Tags", {}).get("multi_select", [])
-    tags_list = [tag['name'] for tag in tags] if tags else []
-
-    # Pour récupérer les status de lecture (à lire, en cours, lu)
-    # status = result["properties"]["Status"]["status"]["name"]
-    status = result["properties"].get("Status", {}).get("status", {}).get("name", "No status")
-
-    # Pour récupérer l'image de couverture 
-    # cover = result["properties"]["Cover"] # don't know what to do with the images for now, we'll see later
-
-    # Récupérer la date de fin de lecture 
-    date_json = result["properties"].get("Date de fin de lecture prévue", {}).get("date", None)
-    if date_json is not None :
-        reading_end_date = date_json["start"]
-        # print(type(reading_end_date)) # date in str format for the moment
-
-    else :
-        reading_end_date = "No date"
-
-    # Pour récupérer les favoris
-    # favorite = result["properties"]["Favoris"]["checkbox"]
-    favorite = result["properties"].get("Favoris", {}).get("checkbox", False) # les élèments de cette colonnes ne peuvent pas être vides (configuré dans notion)
-
-    # Pour récupérer la notation sur 10
-    # rating_number = result["properties"]["Note sur 10"]["number"]
-    rating_number = result["properties"].get("Note sur 10", {}).get("number", None)
-
-    # Pour récupérer la notation en étoiles
-    # stars_rating = result["properties"]["Etoiles"]["formula"]["string"] 
-    stars_rating = result["properties"].get("Etoiles", {}).get("formula", {}).get("string", "☆☆☆☆☆") # les élèments de cette colonne ne peuvent pas être vides (configurée dans notion)
-
-    # Récupérer les commentaires
-    # comments = result["properties"]["Commentaire"]["rich_text"][0]["text"]["content"]
-    comments_property = result["properties"].get("Commentaire", {}).get("rich_text", [])
-    comments = comments_property[0].get("text", {}).get("content", "No comment") if comments_property else "No comment"
 
 
 # Charger les données depuis le fichier JSON
@@ -138,6 +90,81 @@ db = client["LivresDB"]
 
 # Créer une collection
 collection = db["Livres"]
-# Fermeture explicite de la connexion
-client.close()
 print("Connexion à MongoDB réussie !")
+
+
+
+for result in response["results"]:
+    
+    # Pour récupérer les titres des livres
+    # title = result["properties"]["Titre"]["title"][0]["text"]["content"]
+    title_property = result["properties"].get("Titre", {}).get("title", [])
+    title = title_property[0].get("text", {}).get("content", "No title") if title_property else "No title"
+
+    # Pour récupérer les auteurs des livres
+    # author = result["properties"]["Auteur"]["rich_text"][0]["text"]["content"]
+    author_property = result["properties"].get("Auteur", {}).get("rich_text", [])
+    author = author_property[0].get("text", {}).get("content", "No author") if author_property else "No author"
+
+    # Pour récupérer les tags/genres des livres
+    tags = result["properties"].get("Tags", {}).get("multi_select", [])
+    tags_list = [tag['name'] for tag in tags] if tags else []
+
+    # Pour récupérer les status de lecture (à lire, en cours, lu)
+    # status = result["properties"]["Status"]["status"]["name"]
+    status = result["properties"].get("Status", {}).get("status", {}).get("name", "No status")
+
+    # Pour récupérer l'image de couverture 
+    # cover = result["properties"]["Cover"] # don't know what to do with the images for now, we'll see later
+
+    # Récupérer la date de fin de lecture 
+    date_json = result["properties"].get("Date de fin de lecture prévue", {}).get("date", None)
+    if date_json is not None :
+        reading_end_date = date_json["start"]
+        # print(type(reading_end_date)) # date in str format for the moment
+
+    else :
+        reading_end_date = "No date"
+
+    # Pour récupérer les favoris
+    # favorite = result["properties"]["Favoris"]["checkbox"]
+    favorite = result["properties"].get("Favoris", {}).get("checkbox", False) # les élèments de cette colonnes ne peuvent pas être vides (configuré dans notion)
+
+    # Pour récupérer la notation sur 10
+    # rating_number = result["properties"]["Note sur 10"]["number"]
+    rating_number = result["properties"].get("Note sur 10", {}).get("number", None)
+
+    # Pour récupérer la notation en étoiles
+    # stars_rating = result["properties"]["Etoiles"]["formula"]["string"] 
+    stars_rating = result["properties"].get("Etoiles", {}).get("formula", {}).get("string", "☆☆☆☆☆") # les élèments de cette colonne ne peuvent pas être vides (configurée dans notion)
+
+    # Récupérer les commentaires
+    # comments = result["properties"]["Commentaire"]["rich_text"][0]["text"]["content"]
+    comments_property = result["properties"].get("Commentaire", {}).get("rich_text", [])
+    comments = comments_property[0].get("text", {}).get("content", "No comment") if comments_property else "No comment"
+
+    # Transformer les données pour MongoDB
+    book = {
+        "Titre": title,
+        "Auteur": author,
+        "Tags": tags_list,
+        "Status": status,
+        "Date_de_fin_lecture": reading_end_date,
+        "Favoris": favorite,
+        "Note_sur_10": rating_number,
+        "Commentaire": comments
+    }
+
+    # Insérer dans MongoDB
+    try:
+        collection.insert_one(book)
+        print(f"Livre ajouté : {book['Titre']}")
+
+    except Exception as e:
+        print(f"Erreur lors de l'ajout : {e}")
+
+
+
+
+# Fermeture explicite de la connexion MongoDB
+client.close()
